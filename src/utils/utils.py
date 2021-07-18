13e849bce9dc12e7ff38a364c4a4095a7df0e3da
shijinyang <63438575+shijinyang@users.noreply.github.com>
2021-07-17 22:05:57 -0400

    add keras implement

---
+++ src/utils/utils.py
@@ -0,0 +1,7 @@
+from functools import reduce
+
+def compose(*funcs):
+    if funcs:
+        return reduce(lambda f, g: lambda *a, **kw: g(f(*a, **kw)), funcs)
+    else:
+        raise ValueError('Composition of empty sequence not supported.')
